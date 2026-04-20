a32c9b397f67000b57acd4f2ff352c6415c818be
vnapoleone <vnapoleone@frba.utn.edu.ar>
2026-04-20 18:46:35 +0000

    Intento corregir el error de los tests

--- operaciones.c
+++ operaciones.c
@@ -11,7 +11,6 @@ int sumar(int a, int b) {
 int restar(int a, int b) {
     return a - b;
 }
-
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 // Implementa la funcion multiplicar, que recibe dos enteros y devuelve su producto. Por ejemplo, multiplicar(3, 4) debería devolver 12.   
 int multiplicar(int a, int b) {
